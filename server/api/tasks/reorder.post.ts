dd7d4bb27208064147494cf88db745c5d85abfcd
zaboulia <zaboulia@uno.edu>
2023-07-31 10:22:03 +0200

    Making reorder do both indexing at once

--- server/api/tasks/reorder.post.ts
+++ server/api/tasks/reorder.post.ts
@@ -3,15 +3,17 @@ import prisma from '../../prisma';
 export default eventHandler(async (event) => {
     const {oldIndex,oldIndexId, newIndex, newIndexId} = await readBody<{oldIndex : number, oldIndexId : string, newIndex : number, newIndexId : string}>(event);
     try{
-        await prisma.task.update({
+        const updateOld = prisma.task.update({
             where : {id : oldIndexId},
             data : {index : newIndex, lastUpdate : new Date(Date.now()) }
         })
 
-        await prisma.task.update({
+        const updateNew = prisma.task.update({
             where : {id : newIndexId},
             data : {index : oldIndex }
         })
+
+        await prisma.$transaction([updateOld, updateNew]);
         return { success: true, error: ""};
     }
     catch(error : any){
